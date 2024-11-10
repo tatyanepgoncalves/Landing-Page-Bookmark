76203733fad361ee5241dae63a1b230eda7d4793
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-11-10 20:45:28 -0300

    :wrench: chore: Configuração da Página

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import Header from "./Components/Header/HEADER"
+import Header from "./Components/Header/Header";
+import Main from "./Components/Main/Main";
+import Footer from "./Components/Footer/Footer";
 
 export default function App() {
   return (
     <div>
       <Header />
+      <Main />
+      <Footer />
     </div>
   )
 }
